1ebe150d30448f9fb95ba857da94bca524c32b12
LeoBlanco123 <leonardo.blanco.pr@gmail.com>
2025-10-13 15:57:43 -0300

    alteração layout da pagina

--- js/script.js
+++ js/script.js
@@ -10,15 +10,19 @@
     window.open(url, '_blank');
   });
 
-  //BOTÃO ESPECIALISTA
-  const especialistaButton = document.getElementById('especialistaButton');
-  especialistaButton.addEventListener('click', function(e) {
+// BOTÃO ESPECIALISTA
+const botoes = document.querySelectorAll(".especialistaButton");
+
+botoes.forEach(botao => {
+  botao.addEventListener('click', function(e) {
     e.preventDefault();
     const mensagem = "Olá, vim através do site e gostaria de mais informações";
     const numero = "5511972722661";
     const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
     window.open(url, '_blank');
+  });
 });
+
 
 //FORM WHATSAPP
 const form = document.getElementById('whatsappForm');
